5a45b0c3664a98125255e9549f46eee16d1e06cb
akshatrajsaxena <119042958+akshatrajsaxena@users.noreply.github.com>
2024-07-09 20:40:50 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import styles from "./App.module.css";
+import { About } from "./components/About/About.jsx";
+import { Contact } from "./components/Contact/Contact.jsx";
+import { Experience } from "./components/Experience/Experience.jsx";
+import { Hero } from "./components/Hero/Hero.jsx";
+import { Navbar } from "./components/Navbar/Navbar.jsx";
+import { Projects } from "./components/Projects/Projects.jsx";
+import { Achievements } from "./components/achievements/achievements.jsx";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Achievements />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
